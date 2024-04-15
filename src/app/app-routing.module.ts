a6d7319c88db10cd522b2cffbaad3bb8708aa73a
Sergio <sergiosanz2004@gmail.com>
2024-04-15 09:30:18 +0200

    terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPagesComponent } from './shared/pages/about-pages/about-pages.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // {
+  //   path: '',
+  //   component: HomePageComponent
+  // },
+  {
+    path: 'about',
+    component: AboutPagesComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'countries'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPagesComponent } from './pages/about-pages/about-pages.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPagesComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HomePageComponent,
+    AboutPagesComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent
+  ]
+})
+export class SharedModule { }
